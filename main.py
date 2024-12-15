c284108dd6e8dd08123315b651321dad889c5dfd
Habibahisham2455 <h.hisham2455@nu.edu.eg>
2024-12-15 18:26:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from collections import Counter
-s=input("enter a string ") 
-# data structure like dictionary , list, set, tuple
-# dictionary : contain key and value {1: 'habiba', 2:'malak'} , so he search with the value if it matches the dictionary values will increment the key 
-# set : store unique values ,but doesnot have counter. Couldnot do an external counter as it may be large number of diffrent characters
-# tupple or string : due to immutability 
-letter_count=Counter(s)
-print(letter_count) #return dictionary with key and value
-result="" # result=0 it will be integer 
-for char,count in letter_count.items():
-    result += f"{char}{count}"
-print(result)
- 
+# the problem if we have multiple of characters are repeated
+s=input("please enter a string : ")
+sorted_s = sorted(s)
+count=1
+result=""
+for i in range(1,len(s)):
+    if sorted_s[i]==sorted_s[i-1]: # if the current character is same as previous one
+        count+=1
+    else:
+        result += f"{count}{sorted_s[i-1]}"
+        count=1 #Restart the count again 
+result += f"{count}{sorted_a[-1]}" # -1 here means to count until the last character ,as it doeanot has any char after that so it willnot be included  
+# cannot do result=any thing because string is immutable plus it will overwrite the previous result
